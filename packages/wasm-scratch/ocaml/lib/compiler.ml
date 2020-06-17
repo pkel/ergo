8844b96f5e26310e3c2fbef35b4e594508999ad6
Patrik Keller <patrik@keller-re.de>
2020-06-17 19:48:33 +0200

    Compile imp libs, functions, assignments and blocks.
    Support for block is not complete. This implementation assumes that
    variable names are unique. This is not true in general.

--- packages/wasm-scratch/ocaml/lib/compiler.ml
+++ packages/wasm-scratch/ocaml/lib/compiler.ml
@@ -1,10 +1,41 @@
+module Index : sig
+  type 'a t
+
+  val create : unit -> 'a t
+  val id : 'a t -> 'a -> int
+  val elements : 'a t -> 'a array
+end = struct
+  type 'a t = ('a, int) Hashtbl.t * int ref
+
+  let create () = Hashtbl.create 7, ref 0
+
+  let id (ht, size) x =
+    match Hashtbl.find_opt ht x with
+    | Some id -> id
+    | None ->
+      let id = !size in
+      Hashtbl.add ht x id;
+      incr size;
+      id
+
+  let elements (ht, size) =
+    let a = Array.make !size None in
+    Hashtbl.iter (fun el id -> a.(id) <- Some el) ht;
+    Array.map Option.get a
+end
+
 open Ergo_lib
 
-type context =
+type global_context =
   { mutable constants : string list
   ; mutable constants_size : int
   ; alloc_p : Ir.global
   ; memory : Ir.memory
+  }
+
+type local_ctx =
+  { locals : char list Index.t
+  ; global : global_context
   }
 
 let create_context () = { constants = []
@@ -19,6 +50,7 @@ let unsupported : type a. string -> a = fun s -> raise (Unsupported s)
 type data = Core.ejson
 type op = Core.ejson_op
 type runtime = Core.ejson_runtime_op
+type imp = Core.imp_ejson
 
 let encode : data -> bytes = function
   | Ejnull ->
@@ -52,9 +84,9 @@ let encode : data -> bytes = function
 
 let const ctx x : Ir.instr =
   let s = Bytes.to_string (encode x) in
-  let offset = ctx.constants_size in
-  ctx.constants <- s :: ctx.constants;
-  ctx.constants_size <- String.length s + ctx.constants_size;
+  let offset = ctx.global.constants_size in
+  ctx.global.constants <- s :: ctx.global.constants;
+  ctx.global.constants_size <- String.length s + ctx.global.constants_size;
   Ir.i32_const' offset
 
 let f_not =
@@ -104,19 +136,56 @@ let op ctx op args : Ir.instr list =
 let rec expr ctx expression : Ir.instr list =
   match (expression : _ Core.imp_expr) with
   | ImpExprError err -> unsupported "expr: error"
-  | ImpExprVar varname -> unsupported "expr: var"
+  | ImpExprVar v -> [Ir.local_get (Index.id ctx.locals v)]
   | ImpExprConst x -> [const ctx x]
   | ImpExprOp (x, args) -> op ctx x (List.map (expr ctx) args)
   | ImpExprRuntimeCall (op, args) -> unsupported "expr: runtime call"
+
+let rec statement ctx stmt : Ir.instr list =
+  match (stmt : _ Core.imp_stmt) with
+  | ImpStmtBlock (vars, stmts) ->
+    (* TODO: This assumes that variable names are unique which is not true in general. *)
+    let defs =
+      List.map (fun (var, value) ->
+          let id = Index.id ctx.locals var in
+          match value with
+          | Some x ->  expr ctx x @ [ Ir.local_set id ]
+          | None -> []
+        ) vars
+    in
+    let body = List.map (statement ctx) stmts in
+    List.concat (defs @ body)
+  | ImpStmtAssign (var, x) ->
+    expr ctx x @ [ Ir.local_set (Index.id ctx.locals var) ]
+  | ImpStmtFor _ -> unsupported "statement: for"
+  | ImpStmtForRange _ -> unsupported "statement: for range"
+  | ImpStmtIf _ -> unsupported "statement: if"
+
+let function_ ctx fn : Ir.func =
+  let Core.ImpFun (arg, stmt, ret) = fn in
+  let locals = Index.create () in
+  let ctx = { global = ctx; locals } in
+  let l_arg = Index.id locals arg in
+  let () = assert (l_arg = 0) in
+  let body =
+    statement ctx stmt @
+    Ir.[ local_get (Index.id locals ret) ]
+  in
+  Ir.(func ~params:[i32] ~result:[i32] body)
 
 let f_start ctx =
   let open Ir in
   func [ i32_const' ctx.constants_size; global_set ctx.alloc_p ]
 
-let module_ ctx funcs : Ir.module_ =
-  { Ir.start = Some (f_start ctx)
-  ; globals = ["alloc_p", ctx.alloc_p]
-  ; memories = ["memory", ctx.memory]
-  ; funcs
-  ; data = [ ctx.memory, 0, String.concat "" (List.rev ctx.constants) ]
-  }
+let imp functions : Wasm.Ast.module_ =
+  let ctx = create_context () in
+  let funcs = List.map (fun (name, fn) ->
+      (Util.string_of_char_list name, function_ ctx fn)) functions
+  in
+  Ir.compile
+    { Ir.start = Some (f_start ctx)
+    ; globals = ["alloc_p", ctx.alloc_p]
+    ; memories = ["memory", ctx.memory]
+    ; funcs
+    ; data = [ ctx.memory, 0, String.concat "" (List.rev ctx.constants) ]
+    }
--- packages/wasm-scratch/ocaml/test/compile.ml
+++ packages/wasm-scratch/ocaml/test/compile.ml
@@ -8,6 +8,7 @@ module Const = struct
   let false_ = ImpExprConst (Ejbool false)
   let true_ = ImpExprConst (Ejbool true)
   let number x = ImpExprConst (Ejnumber x)
+  let string x = ImpExprConst (Ejstring (Util.char_list_of_string x))
 end
 
 let expressions =
@@ -17,21 +18,25 @@ let expressions =
   ; true_
   ; false_
   ; number 3.14
-  ; ImpExprConst (Ejstring ['e'; 'r'; 'g'; 'o'])
-  ; ImpExprConst (Ejstring [])
+  ; string "ergo"
+  ; string ""
   ; ImpExprOp (EJsonOpNot, [false_])
   ; ImpExprOp (EJsonOpNot, [true_])
   ]
 
 let m =
-  let ctx = Compiler.create_context () in
-  let funcs =
+  let imp : Compiler.imp =
     List.mapi (fun i e ->
-        "f" ^ Int.to_string i, Ir.(func ~result:[i32] (Compiler.expr ctx e))
+        let open Core in
+        let name = "f" ^ Int.to_string i
+        and arg, ret = ['a'], ['r']
+        in
+        let stmt = ImpStmtAssign (ret, e)
+        in Util.char_list_of_string name, ImpFun (arg, stmt, ret)
       ) expressions
   in
-  Compiler.module_ ctx funcs
+  Compiler.imp imp
 
 let () =
-  Wasm.Print.module_ stdout 72 (Ir.compile m)
+  Wasm.Print.module_ stdout 72 m
 
